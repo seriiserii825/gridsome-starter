b380e5b6455ff4fc5b1a6cc7e196e34e290d89d9
serii <seriiburduja@gmail.com>
2020-11-03 17:47:01 +0200

    add content folder

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -27,6 +27,17 @@
 </template>
 
 <script>
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", user => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
+</script>
+<script>
 
 export default {
   metaInfo: {
